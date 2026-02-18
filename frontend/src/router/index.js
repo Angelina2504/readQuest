9b2c932b029b959650909e7c78ce0425047cf967
Angelina2504 <angieso91@gmail.com>
2026-02-18 16:38:17 +0100

    first step view aboutus

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import Register from '@/views/auth/Register.vue'
+import AboutUs from '@/views/AboutUs.vue'
 
 const routes = [
   { path: '/', component: HomeView, name:'Accueil'},
-  { path: '/signin', component: Register, name:'Inscription'}
+  { path: '/signin', component: Register, name:'Inscription'},
+  { path: '/about', component: AboutUs, name:'Notre histoire'}
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/AboutUs.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="button-group">
+        <router-link 
+          v-for="about in aboutLinks" 
+          :key="about.text" 
+          :to="about.path" 
+          class="custom-button"
+        >
+          {{ about.text }}
+        </router-link>
+    </div>
+
+    <div class="img-deco">
+       <img src="@/assets/deco/aboutLessBG.png" alt="Illustration ReadQuest" />
+     </div>
+</template>
+
+<script setup>
+const aboutLinks = ([
+
+    { text: 'ReadQuest', path: '/signin', isHovered: false },
+    { text: 'Livresse podcast', path: '/login', isHovered: false },
+
+])
+</script>
+
+<style scoped>
+</style>
