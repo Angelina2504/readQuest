31c639cab0b019266c82904704342c6eca4f5cdf
Angelina2504 <angieso91@gmail.com>
2026-02-25 16:40:54 +0100

    create first step to explain quest

---
+++ frontend/src/components/information/LevelRules.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script setup>
+    const levelRules = ([
+        {h3:'Fable - niveau 1', src: "@/assets/deco/lvl1Fable.png", text: "La découverte accessible. C’est votre point d’entrée. Ces quêtes proposent d'explorer de nouveaux horizons sans contrainte majeure."},
+        {h3:'Légende - niveau 2', src: "@/assets/deco/lvl2Legende.png", text: "L’exigence s’affirme. Le défi demande un peu plus d’attention. On attend de vous une immersion plus longue ou une action plus réfléchie. C’est le niveau idéal pour ceux qui veulent approfondir un sujet sans encore s'y perdre."},
+        {h3:'Mythe - niveau 3', src: "@/assets/deco/lvl3Mythe.png", text: "L’immersion totale. Ici, vous changez de dimension. Ce rang marque les quêtes qui exigent une réflexion, une recherche ou une confrontation à des thématiques plus denses. On ne survole plus, on plonge au cœur du sujet."},
+        {h3:'Odysée - niveau 4 - ', src: "", text: "Le défi d’endurance. Préparez-vous pour le temps long. L'Odyssée récompense la persévérance : sagas monumentales, études croisées ou parcours complexes. Seule votre détermination vous permettra de franchir la ligne d'arrivée."},
+        {h3:'Anthologie - niveau 5  ', src: "", text: "Le sommet de l’expertise. Le rang des maîtres. Une quête Anthologie est une quête d'exception : elle demande de l'érudition, de la rareté ou une vision globale du sujet. C’est la consécration ultime pour ceux qui ont exploré chaque recoin d'un thème."}
+    ])
+</script>
+
+<style scoped>
+
+</style>
