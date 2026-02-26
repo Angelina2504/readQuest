44d62625dad30ada56e21f5a41e281f356cd4284
Angelina2504 <angieso91@gmail.com>
2026-02-26 16:41:29 +0100

    Base modal = component reusable for pop up

---
+++ frontend/src/components/information/BaseModal.vue
@@ -0,0 +1,11 @@
+<template>
+ <img class="button-question" :src="'./src/assets/icons/ptInterogationBlack.png'" alt="boutton image pour information"></img>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
