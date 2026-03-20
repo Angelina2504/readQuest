b70743f58bf59b3ad15fae19a51003053d809046
Angelina2504 <angieso91@gmail.com>
2026-03-20 15:27:30 +0100

    :bust_in_silhouette: AppFixtures for testing

---
+++ backend/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUserAlias('Toto');
+        
+        $password = $this->hasher->hashPassword($user, 'password1234');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
