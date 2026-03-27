1eb3b355e09072f810a851daff23fa6d39fcd773
Angelina2504 <angieso91@gmail.com>
2026-03-27 11:57:58 +0100

    :rocket: api: add axios interceptor to automatically attach JWT Bearer token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,23 @@ import axios from 'axios';
 const api = axios.create({
     baseURL:'http://localhost:8080/api',
     headers: { "Content-Type": 'application/json', }
-})
+});
+
+//Automation of token sending for each future request.
+api.interceptors.request.use(
+    (config)=> {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            // Celui qui porte ce jeton a le droit d'entrer
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+    
+);
 
 export default api;
