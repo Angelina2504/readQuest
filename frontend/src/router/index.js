3834e284dcdf1f2d4895272da6799c8dfbbe53ac
Angelina2504 <angieso91@gmail.com>
2026-03-27 15:00:40 +0100

    :shield: router: implement navigation guards and AdminView with role-based access

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import Register from '@/views/auth/Register.vue';
+import { authService } from '@/services/authService';
 import AboutUs from '@/views/AboutUs.vue';
-import Login from '@/views/auth/LoginAuth.vue';
+import AdminView from '@/views/adminSystem/AdminView.vue';
 import Contact from '@/views/ContactUs.vue';
+import HomeView from '@/views/HomeView.vue';
+import Login from '@/views/auth/LoginAuth.vue';
+import Profil from '@/views/ProfilUser.vue';
 import Quest from '@/views/Quest.vue';
-import Profil from '@/views/ProfilUSer.vue';
+import Register from '@/views/auth/Register.vue';
 
 const routes = [
+  // visitor
   { path: '/', component: HomeView, name:'Accueil'},
-  { path: '/signin', component: Register, name:'Inscription'},
   { path: '/about', component: AboutUs, name:'Notre histoire'},
-  { path: '/login', component: Login, name:'Se connecter'},
   { path: '/contact', component: Contact, name:'Contact'},
-  { path: '/profil', component: Profil, name:'Profil'},
-  { path: '/quests', component: Quest, name:'Quête'},
-  
+  { path: '/login', component: Login, name:'Se connecter'},
+  { path: '/signin', component: Register, name:'Inscription'},
+
+  // user
+  { path: '/profil', component: Profil, name:'Profil', meta: {requiresAuth : true}},
+  { path: '/quests', component: Quest, name:'Quête', meta: {requiresAuth : true}},
+
+  //admin
+  { path: '/admin', compoment: AdminView, name:"Admin", meta:{requiresAdmin : true}},
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  const isConnected = authService.isAuthenticated();
+  const isAdmin = authService.isAdmin();
+
+  if (to.meta.requiresAdmin) {
+    if (isConnected && isAdmin) {
+      next();
+    } else {
+      next({ name: 'Accueil' });
+    }
+  } 
+  else if (to.meta.requiresAuth) {
+    if (isConnected) {
+      next();
+    } else {
+      next({ name: 'Se connecter' });
+    }
+  } 
+  else {
+    next();
+  }
+});
 
 export default router
---
+++ frontend/src/views/adminSystem/AdminView.vue
@@ -0,0 +1,11 @@
+<template>
+<h1 class="title-profil">ADMIN</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
